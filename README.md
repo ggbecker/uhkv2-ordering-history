60213989a3212e82e2e9badc7cf49e65c1faba4c
Gabriel Becker <ggasparb@redhat.com>
2021-11-13 02:08:11 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+Date | Current Order | Last Order
+--- | --- | --- 
+2021-10-31 | 7947 | 11458
---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+URL = 'https://ultimatehackingkeyboard.com/delivery-status'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+post_content = soup.find('div', class_='post-content')
+if post_content:
+    text_list = post_content.find_all('p')
+
+    text=""
+    for phrase in text_list:
+        text+=phrase.text
+
+    try:
+        first_order_to_be_shipped = re.search('to be shipped is #(.+?),', text).group(1)
+    except AttributeError:
+        first_order_to_be_shipped = ''
+
+    try:
+        last_order = re.search(' the last order is #(.+?),', text).group(1)
+    except AttributeError:
+        last_order = ''
+
+
+    try:
+        updated_date = re.search('and it was updated on (.+?)\.', text).group(1)
+    except AttributeError:
+        updated_date = ''
+
+    print(" | ".join([updated_date, first_order_to_be_shipped, last_order]))
+    # print("first_order_to_be_shipped", first_order_to_be_shipped)
+    # print("last_order", last_order)
+    # print("updated_date", updated_date)
